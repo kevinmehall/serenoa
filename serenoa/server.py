fcb55150ae4d1e84949914b5d873bdc95957196e
Kevin Mehall <km@kevinmehall.net>
2012-07-25 20:36:48 -0600

    server: refresh after 10s

--- serenoa/server.py
+++ serenoa/server.py
@@ -1,12 +1,22 @@
 import BaseHTTPServer, os, time
 
+refresh_timeout = 10
+
 class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
 	def do_GET(self):
+		context = self.server.context
+
+		# refresh the tree if the timeout has passed
+		if self.server.refresh_time < time.time() - refresh_timeout:
+			print "Refreshing...", self.server.refresh_time, time.time()
+			context.run()
+			self.server.refresh_time = time.time()
+
 		try:
-			f = self.context.files[self.path[1:]]
+			f = context.files[self.path[1:]]
 		except KeyError:
 			try:
-				f = self.context.files[os.path.join(self.path[1:], 'index')]
+				f = context.files[os.path.join(self.path[1:], 'index')]
 			except KeyError:
 				self.send_error(404, "Not found")
 				return
@@ -18,8 +28,9 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
 				
 
 def serve(context, port):
-	Handler.context = context
 	s = BaseHTTPServer.HTTPServer(('', port), Handler)
+	s.context = context
+	s.refresh_time = 0
 	s.serve_forever()
 	
 if __name__ == '__main__':
@@ -27,6 +38,5 @@ if __name__ == '__main__':
 	import core
 	
 	c = core.Context(sys.argv[1])
-	c.run()
 	serve(c, 8080)
 
