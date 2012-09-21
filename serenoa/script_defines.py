d3fdaad0c74cfe7e440a66260ff559fd2c740c61
Kevin Mehall <km@kevinmehall.net>
2012-09-21 15:30:40 -0600

    Return glob paths relative to the base

--- serenoa/script_defines.py
+++ serenoa/script_defines.py
@@ -4,5 +4,10 @@ from serenoa.mustache_template import MustacheTemplate
 from serenoa.cmd_file import CommandFile, LessFile, CoffeescriptFile
 
 import glob2
-def glob(pattern):
-	return glob2.iglob(abspath(pattern, False))
+def glob(*patterns):
+	base = abspath('', False) + '/'
+	matches = []
+	for pattern in patterns:
+		matches += glob2.iglob(abspath(pattern, False))
+	return (m.replace(base, '', 1) for m in matches)
+
